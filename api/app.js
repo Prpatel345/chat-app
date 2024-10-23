89b5af5d5ed9254808b1baf106fc02506433f5f5
Prpatel345 <pbr33600@gmail.com>
2024-10-23 19:35:31 +0530

    Removed socket changes and ejs

--- api/app.js
+++ api/app.js
@@ -1,27 +1,12 @@
 const express = require("express");
-const path = require("path");
-const { initSocket } = require("./socket");
 const app = express();
 
-const port = process.env.PORT || 8000; 
-
-app.set("view engine", "ejs");
-app.use("views", express.static(path.join(__dirname, "views")));
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-  res.render("login");
+  res.status(200).json({ message: "Application running as tiptop" });
 });
 
-app.get("/chat", (req, res) => {
-  res.render("chat");
-});
-
-app.get("/chat/:id", (req, res) => {
-  res.render("chat");
-});
-
-const server = app.listen(port, () =>
+app.listen(port, () =>
   console.log("listening on http://localhost:8000")
 );
-
-initSocket(server);
